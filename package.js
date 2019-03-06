be4e2f275e40ad02edced07ac32d8ffd29c8a77e
Irelander <iiitttiii@naver.com>
2019-03-06 09:43:03 +0900

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "irelander:mssql",
   summary: "mssql wrapper: non-reactive SQL Server package fork from emgee:mssql",
-  version: "1.0.8",
+  version: "1.0.80",
   git: "https://github.com/Irelander/mssql.git",
   documentation: "README.md"
 });
